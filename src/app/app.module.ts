ba7fc3fb97c6706fe2bedf78c02a6f022a36f9da
Nicolas Chaima <nikochaima72@gmail.com>
2019-11-13 04:58:28 -0300

    Generando componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+
 import { AppComponent } from './app.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegistrarComponent } from './pages/registrar/registrar.component';
+import { PrivadoComponent } from './pages/privado/privado.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    NavbarComponent,
+    LoginComponent,
+    RegistrarComponent,
+    PrivadoComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
