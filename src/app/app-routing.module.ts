3d3af6596d5519e0dff690a88ad54b077af09ee1
Nicolas Chaima <nikochaima72@gmail.com>
2019-11-13 16:42:07 -0300

    Maquetacion de paginas + Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegistrarComponent } from './pages/registrar/registrar.component';
+import { PrivadoComponent } from './pages/privado/privado.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'inicio', component: InicioComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'registrar', component: RegistrarComponent },
+  { path: 'privado', component: PrivadoComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'inicio' }
+  // { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
