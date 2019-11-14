ee67ede7cc9d4961eb74468eec8a264721e8e9a1
Nicolas Chaima <nikochaima72@gmail.com>
2019-11-13 21:37:12 -0300

    Creado servicio de Firebase + Registro de usuario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+
 
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
-
 import { AppComponent } from './app.component';
 import { InicioComponent } from './pages/inicio/inicio.component';
 import { LoginComponent } from './pages/login/login.component';
@@ -12,10 +12,14 @@ import { RegistrarComponent } from './pages/registrar/registrar.component';
 import { PrivadoComponent } from './pages/privado/privado.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 
-import { AngularFireModule } from '@angular/fire';
+
 import { environment } from "../environments/environment";
+import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireAuthModule } from '@angular/fire/auth';
+
+
+import { FirebaseService } from './services/firebase.service';
 
 @NgModule({
   declarations: [
@@ -30,12 +34,16 @@ import { AngularFireAuthModule } from '@angular/fire/auth';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
 
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFirestoreModule,
     AngularFireAuthModule,
   ],
-  providers: [],
+  providers: [
+    FirebaseService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/pages/registrar/registrar.component.ts
+++ src/app/pages/registrar/registrar.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormArray } from "@angular/forms";
+import { Observable } from 'rxjs';
+import { UsuarioModel } from 'src/app/models/usuario.model';
+import { FirebaseService } from 'src/app/services/firebase.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-registrar',
@@ -7,9 +13,65 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrarComponent implements OnInit {
 
-  constructor() { }
+  usuario: UsuarioModel = {
+    email: '',
+    password: ''
+  };
+  form: FormGroup;
+  hayError = false;
+  emailExiste = false;
+
+  constructor(
+    // tslint:disable-next-line:variable-name
+    private _firebase: FirebaseService,
+    // tslint:disable-next-line:variable-name
+    private _router: Router
+  ) {
+    this.form = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.pattern('^[^@]+@[^@]+\.[a-zA-Z]{2,}$')]),
+      password1: new FormControl(),
+      password2: new FormControl()
+    });
+    this.form.controls.password1.setValidators([
+      Validators.required,
+      Validators.minLength(8),
+      this.noIgual.bind(this.form)
+    ]);
+    this.form.controls.password2.setValidators([
+      Validators.required,
+      this.noIgual.bind(this.form)
+    ]);
+  }
 
   ngOnInit() {
+  }
+
+  noIgual(control: FormControl): {[s: string]: boolean} {
+    // tslint:disable-next-line:prefer-const
+    let forma: any = this;
+    if (control.value !== forma.controls.password1.value) {
+      return {
+        noIgual: true
+      };
+    }
+    return null;
+  }
+
+
+
+  ngSubmitAddUser() {
+    this.hayError = false;
+    this.emailExiste = false;
+
+    this._firebase.registerUser(this.usuario).then((res) => {
+      this._router.navigate(['/privado']);
+    }).catch((err) => {
+      this.hayError = true;
+      console.log(err.code);
+      if (err.code === 'auth/email-already-in-use') {
+        this.emailExiste = true;
+      }
+    });
   }
 
 }
