827ae6110c5303e891bd6035c20855e4d53d0910
Nicolas Chaima <nikochaima72@gmail.com>
2019-11-14 23:54:38 -0300

    Agregado Login + Logout

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FirebaseService } from "src/app/services/firebase.service";
+
+import { AngularFirestore } from "@angular/fire/firestore";
+import { AngularFireAuth } from "@angular/fire/auth";
+import * as firebase from "firebase/app";
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  selector: "app-navbar",
+  templateUrl: "./navbar.component.html",
+  styleUrls: ["./navbar.component.css"]
 })
 export class NavbarComponent implements OnInit {
+  public isLogin: boolean;
+  public nombreUsuario: string;
+  public emailUsuario: string;
 
-  constructor() { }
+  constructor(
+    // tslint:disable-next-line:variable-name
+    private _firebase: FirebaseService,
+    // tslint:disable-next-line:variable-name
+    private _auth: AngularFireAuth
+  ) {}
 
   ngOnInit() {
+    this._auth.auth.onAuthStateChanged(user => {
+      if (user) {
+        this.isLogin = true;
+        this.nombreUsuario = user.displayName;
+        this.emailUsuario = user.email;
+      } else {
+        this.isLogin = false;
+      }
+    });
   }
 
+  logOut() {
+    this._firebase.logOut();
+  }
 }
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { UsuarioModel } from "src/app/models/usuario.model";
+import { FormGroup, FormControl, Validators, FormArray } from "@angular/forms";
+import { FirebaseService } from "src/app/services/firebase.service";
+import { Router } from "@angular/router";
+import { Observable } from "rxjs";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"]
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  usuario: UsuarioModel = {
+    email: "",
+    password: ""
+  };
+  form: FormGroup;
+  hayError = false;
 
-  ngOnInit() {
+  constructor(
+    // tslint:disable-next-line:variable-name
+    private _firebase: FirebaseService,
+    // tslint:disable-next-line:variable-name
+    private _router: Router
+  ) {
+    this.form = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.pattern('^[^@]+@[^@]+\.[a-zA-Z]{2,}$')]),
+      password1: new FormControl('', [Validators.required]),
+    });
   }
 
+  ngOnInit() {}
+
+  ngSubmitLogIn() {
+    this.hayError = false;
+    this._firebase.logIn(this.usuario).then((res) => {
+      this._router.navigate(['/privado']);
+    }).catch((err) => {
+      this.hayError = true;
+    });
+  }
 }
